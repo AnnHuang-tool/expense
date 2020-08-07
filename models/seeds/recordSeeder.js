439f516715752b4c9018f8db69dc1752c7be793f
LIN <yi19930716@gmail.com>
2020-08-07 17:13:01 +0800

    fix: input date

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -6,31 +6,31 @@ db.once('open', () => {
     {
       name: '公車',
       category: '交通出行',
-      date: '2020/7/16',
+      date: '2020-07-16',
       amount: 50
     },
     {
       name: '午餐',
       category: '餐飲食品',
-      date: '2020/7/15',
+      date: '2020-07-15',
       amount: 60
     },
     {
       name: '電動',
       category: '休閒娛樂',
-      date: '2020/7/17',
+      date: '2020-07-17',
       amount: 500
     },
     {
       name: '掃把',
       category: '家居物業',
-      date: '2020/7/17',
+      date: '2020-07-17',
       amount: 100
     },
     {
       name: '糖果',
       category: '其他',
-      date: '2020/7/17',
+      date: '2020-07-17',
       amount: 100
     }
   )
