6d9ea522a4f0b3cb929db1581628967a63abe7fa
LIN <yi19930716@gmail.com>
2020-08-26 15:25:29 +0800

    fix: categorySeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -8,7 +8,7 @@ db.once('open', () => {
   const categoryName = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
   Promise.all(
     categoryName.map(category => {
-      return Category.create({ category })
+      return Category.create({ categoryName: category })
     })
   )
     .then(() => {
