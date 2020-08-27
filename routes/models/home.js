b04d9b610c0bd738eb0680d368041b1030a5c31b
LIN <yi19930716@gmail.com>
2020-08-27 11:07:35 +0800

    feat: add year select

--- routes/models/home.js
+++ routes/models/home.js
@@ -4,6 +4,9 @@ const Record = require('../../models/record.js')
 const Category = require('../../models/category.js')
 const Month = require('../../models/month.js')
 const handlebars = require('handlebars')
+const thisYear = new Date().toLocaleString().slice(0, 4)
+const beforeYear = Number(thisYear) - 5
+
 
 handlebars.registerHelper('equal', (category1, category2, options) => {
   if (category1 === category2) {
@@ -14,6 +17,10 @@ handlebars.registerHelper('equal', (category1, category2, options) => {
 })
 
 router.get('/', (req, res) => {
+  const years = []
+  for (let i = 0; i <= 5; i++) {
+    years.push({ year: beforeYear + i })
+  }
   const userId = req.user._id
   Month.find()
     .lean()
@@ -29,7 +36,7 @@ router.get('/', (req, res) => {
               for (let i = 0; i < record.length; i++) {
                 totalAmount += record[i].amount
               }
-              res.render('index', { record, categories, totalAmount, months })
+              res.render('index', { record, categories, totalAmount, thisYear, years, months })
             })
             .catch(error => console.log(error))
         })
@@ -40,19 +47,33 @@ router.get('/', (req, res) => {
 })
 
 router.get('/filter', (req, res) => {
+  const years = []
+  let { category, month, year } = req.query
   const userId = req.user._id
   let filter = { userId }
-  let { category, month } = req.query
+  for (let i = 0; i <= 5; i++) {
+    years.push({ year: beforeYear + i })
+  }
   if (month) {
     if (month.length === 1) {
       month = `0${month}`
     }
   }
-  if (month && category) {
-    filter.date = { $gte: `2020-${month}-01`, $lte: `2020-${month}-31` }
+  if (month && category && year) {
+    filter.date = { $gte: `${year}-${month}-01`, $lte: `${year}-${month}-31` }
     filter.category = category
+  } else if (month && category) {
+    filter.date = { $regex: `${month}` }
+    filter.category = category
+  } else if (category && year) {
+    filter.date = { $regex: `${year}` }
+    filter.category = category
+  } else if (month && year) {
+    filter.date = { $gte: `${year}-${month}-01`, $lte: `${year}-${month}-31` }
+  } else if (year) {
+    filter.date = { $regex: `${year}` }
   } else if (month) {
-    filter.date = { $gte: `2020-${month}-01`, $lte: `2020-${month}-31` }
+    filter.date = { $regex: `${month}` }
   } else if (category) {
     filter.category = category
   }
@@ -70,7 +91,7 @@ router.get('/filter', (req, res) => {
               for (let i = 0; i < record.length; i++) {
                 totalAmount += record[i].amount
               }
-              res.render('index', { record, categories, totalAmount, category, months, month: Number(month) })
+              res.render('index', { record, categories, totalAmount, category, thisYear, years, year: Number(year), months, month: Number(month) })
             })
             .catch(error => console.log(error))
         })
